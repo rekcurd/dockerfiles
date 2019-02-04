5830f9a2cf0b1614fcf8f1fc9254ce492bdfd7f8
Hattori Keigo <keigoht@gmail.com>
2019-02-04 10:11:36 +0900

    Rename

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # dockerfiles
-This repository stores Dockerfiles for Drucker.
+This repository stores Dockerfiles for Rekcurd.
 
 ## Official docker repository
 https://hub.docker.com/r/rekcurd/rekcurd/
--- drucker-python/entrypoint.sh
+++ drucker-python/entrypoint.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 # You have to set env. Env can be set from Kubernetes Pod boot script.
-#   DRUCKER_SERVICE_GIT_URL="https://github.com/yourproject/yourgit.git"
-#   DRUCKER_SERVICE_GIT_BRANCH="master"
-#   DRUCKER_SERVICE_BOOT_SHELL="start.sh"
+#   REKCURD_SERVICE_GIT_URL="https://github.com/yourproject/yourgit.git"
+#   REKCURD_SERVICE_GIT_BRANCH="master"
+#   REKCURD_SERVICE_BOOT_SHELL="start.sh"
 
 
-ECHO_PREFIX="[drucker-settings]: "
+ECHO_PREFIX="[rekcurd-settings]: "
 echo "$ECHO_PREFIX Start.."
 
 
 set -e
 set -u
 
-# git clone your-drucker.
-git clone --depth 1 --recursive -b ${DRUCKER_SERVICE_GIT_BRANCH} ${DRUCKER_SERVICE_GIT_URL} /usr/local/src/drucker
-cd /usr/local/src/drucker
-sh ${DRUCKER_SERVICE_BOOT_SHELL}
+# git clone your-rekcurd.
+git clone --depth 1 --recursive -b ${REKCURD_SERVICE_GIT_BRANCH} ${REKCURD_SERVICE_GIT_URL} /usr/local/src/rekcurd
+cd /usr/local/src/rekcurd
+sh ${REKCURD_SERVICE_BOOT_SHELL}
